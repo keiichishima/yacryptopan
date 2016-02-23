759828ece1efa54a219ae0e89dd4807c72d8da36
diekmann <diekmann@net.in.tum.de>
2016-02-23 11:30:35 +0100

    anonymizing once again if mapped to special purpose range

--- ipaddresscrypto.py
+++ ipaddresscrypto.py
@@ -24,28 +24,54 @@ class IPAddressCrypt(object):
     """
     def __init__(self, key):
         self.cp = CryptoPAn(key)
+    
+    def get_special_purpose_net(self, ip):
+        ip = netaddr.IPAddress(ip)
+        if ip.version == 4:
+            for net in special_purpose_ipv4:
+                if ip in net:
+                    return net
+            return None
+        elif ip.version == 6:
+            #TODO
+            return None
+        assert False
         
-    def is_special_purpose_ipv4(self, ip):
-        for net in special_purpose_ipv4:
-            if ip in net:
-                return True
-        return False
+    def is_special_purpose(self, ip):
+        return (self.get_special_purpose_net(ip) is not None)
     
     def do_not_anonymize(self, ip):
         ip = netaddr.IPAddress(ip)
         if ip.version == 4:
-            return self.is_special_purpose_ipv4(ip)
+            return self.is_special_purpose(ip)
         else:
             return False #IPv6 addresses can have MACs embedded
             #be super conservative and anonymize them all
-        
+    
+    def __map_to_special_purpose_net(self, ip, special_net):
+        #TODO: only support ipv4. there should be a library function for this?
+        #assert that host-bits of net are all zero. Will fail for IPv6
+        assert ((int(special_net.ip) << special_net.prefixlen) & 0xFFFFFFFF) == 0
+        ip = int(special_net.ip) + (int(netaddr.IPAddress(ip)) % 2^(32-special_net.prefixlen))
+        ip = netaddr.IPAddress(ip)
+        return ip
+    
     def anonymize(self, ip):
         if self.do_not_anonymize(ip):
             #TODO anonymize but completely keep the prefix (i.e. only anonymize the least significant bits)
             return ip
         else:
             ip_anonymized = self.cp.anonymize(ip)
-            if self.do_not_anonymize(ip_anonymized):
-                #TODO: anonymize again until we are in a `good` range?
-                printStdErr("WARNING: anonymized ip address mapped to special-purpose address range. Please consider re-running with different key")
+            # if anonymized IP is accidentally mapped into a special purpose 
+            # range, try to map it to a different range
+            if self.is_special_purpose(ip_anonymized):
+                printStdErr("INFO: anonymized ip %s mapped to "
+                            "special-purpose address range. retrying" % ip)
+                #anonymize once again
+                ip_anonymized = self.cp.anonymize(ip_anonymized)
+                if self.is_special_purpose(ip_anonymized):
+                    printStdErr("WARNING: anonymized ip address mapped to "
+                                "special-purpose address range. Please consider "
+                                "re-running with different key")
             return ip_anonymized
+
